67159d380e548f23f265dbcf70036eec6838e82e
titusefferian <kiritosato@gmail.com>
2024-06-06 22:40:15 +0900

    delete everything

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -12,32 +12,32 @@ const backend = defineBackend({});
 // this is aws amplify gen 2
 // how can i spawn a new vpc using aws cdk here?
 
-const customResourceStack = backend.createStack("MyCustomResources");
+// const customResourceStack = backend.createStack("MyCustomResources");
 
-const myVpc = new ec2.Vpc(customResourceStack, "testing-vpc", {
-  cidr: "10.0.0.0/16",
-  subnetConfiguration: [
-    {
-      cidrMask: 24,
-      name: "PrivateSubnet",
-      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-    },
-  ],
-});
+// const myVpc = new ec2.Vpc(customResourceStack, "testing-vpc", {
+//   cidr: "10.0.0.0/16",
+//   subnetConfiguration: [
+//     {
+//       cidrMask: 24,
+//       name: "PrivateSubnet",
+//       subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+//     },
+//   ],
+// });
 
-const fn = new lambda.Function(customResourceStack, "helloWorldFunc", {
-  runtime: lambda.Runtime.NODEJS_18_X,
-  code: lambda.Code.fromInline(`
-    exports.handler = async (event) => {
-          return {
-            statusCode: 200,
-            body: JSON.stringify('Hello, World!'),
-          };
-        };
-    `),
-  handler: "index.handler",
-  vpc: myVpc,
-  vpcSubnets: {
-    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-  },
-});
+// const fn = new lambda.Function(customResourceStack, "helloWorldFunc", {
+//   runtime: lambda.Runtime.NODEJS_18_X,
+//   code: lambda.Code.fromInline(`
+//     exports.handler = async (event) => {
+//           return {
+//             statusCode: 200,
+//             body: JSON.stringify('Hello, World!'),
+//           };
+//         };
+//     `),
+//   handler: "index.handler",
+//   vpc: myVpc,
+//   vpcSubnets: {
+//     subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+//   },
+// });
