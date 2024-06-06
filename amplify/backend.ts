7876abf8857efd4b0cd9d932dd45706d89dbb7de
titusefferian <kiritosato@gmail.com>
2024-06-06 22:23:38 +0900

    test lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,5 +1,6 @@
-import { defineBackend } from '@aws-amplify/backend';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { defineBackend } from "@aws-amplify/backend";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 // import { auth } from './auth/resource';
 // import { data } from './data/resource';
 
@@ -11,8 +12,23 @@ const backend = defineBackend({});
 // this is aws amplify gen 2
 // how can i spawn a new vpc using aws cdk here?
 
-const customResourceStack = backend.createStack('MyCustomResources');
+const customResourceStack = backend.createStack("MyCustomResources");
 
-new ec2.Vpc(customResourceStack, 'testing-vpc',{
-  cidr: '10.0.0.0/16',
-})
+const fn = new lambda.Function(customResourceStack, "helloWorldFunc", {
+  runtime: lambda.Runtime.NODEJS_18_X,
+  code: lambda.Code.fromInline(`
+    exports.handler = async (event) => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello, World!'),
+          };
+        };
+    `),
+  handler: "index.handler",
+});
+fn.addFunctionUrl({
+  authType: lambda.FunctionUrlAuthType.NONE,
+});
+new ec2.Vpc(customResourceStack, "testing-vpc", {
+  cidr: "10.0.0.0/16",
+});
